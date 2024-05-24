bd19221c5c2e3f173cd57daf8177d21215831d55
Momezz <julgomez14gmail.com>
2024-05-23 20:41:29 -0500

    adaptation to the financial item model

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 function configExpess(app) {
+  app.use(cors());
   app.use(express.json());
   app.use(morgan('dev'));
 }
